46228c248337312100fa1af134fc3eceed7acbca
Adelson Neto <adelsonneto55@gmail.com>
2020-11-19 20:13:20 -0300

    Final project

---
+++ script.js
@@ -0,0 +1,7 @@
+function hover(element) {
+  element.setAttribute('src', 'img/insta2.jpeg');
+}
+src="img/instagram-celular.png"
+function unhover(element) {
+  element.setAttribute('src', 'img/instagram-celular.png');
+}
